2398c06f582d83aa11f482d403695b572fa65487
GitHub Action <action@github.com>
2020-11-16 15:21:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhiiw/codingtimehelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zhiiw/codingtimehelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhiiw.codingtimehelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhiiw.codingtimehelper.MyBundle
 
 class MyApplicationService {
 
